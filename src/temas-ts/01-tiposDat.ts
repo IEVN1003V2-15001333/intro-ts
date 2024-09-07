5a6285305ed393ad8b7b43ee9d73ad97a9dec2b2
Antonio Ornelas Caudillo <15001333@alumnos.utleon.edu.mx>
2024-09-06 18:57:32 -0600

    actividad 01 02

---
+++ src/temas-ts/01-tiposDat.ts
@@ -0,0 +1,24 @@
+console.log('Hola Mundo')
+
+var num1=38
+let num2=29
+const num3=10
+
+let sum = num1 + num2 + num3;
+
+let nombre:string
+let num:number
+let activo:boolean
+
+nombre = 'mario'
+num=32
+activo=true
+
+let matricula:number|string|boolean
+
+matricula=1234567
+matricula='A56FD77'
+
+
+
+console.log('el numero es', sum);
---
+++ src/temas-ts/02-interfaz.ts
@@ -0,0 +1,13 @@
+interface Alumno{
+    nombre:string;
+    edad:number;
+    email:string;
+    calificacion?:number;
+}
+
+const alumno:Alumno={
+    nombre:'mario',
+    edad:23,
+    email:'mario@gmail.com',
+    
+}
